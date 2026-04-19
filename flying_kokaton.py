a0a98696831b97590ac386465b3254b5734fcf64
koseki <c0a24090da@edu.teu.ac.jp>
2026-04-19 20:09:04 +0900

    課題3:gitを利用したファイル名の変更

---
+++ flying_kokaton.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    bg_img2 = pg.transform.flip(bg_img,True,False) #練習8
+    kk_img = pg.image.load("fig/3.png") #練習3
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect() #　練習10-1
+    kk_rct.center = 300,200
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        
+        key_list = pg.key.get_pressed()
+        kk_move = [-1, 0]
+
+        if key_list[pg.K_UP]:
+            kk_move[1] -= 1
+        if key_list[pg.K_DOWN]:
+            kk_move[1] += 1
+        if key_list[pg.K_LEFT]:
+            kk_move[0] -= 1
+        if key_list[pg.K_RIGHT]:
+            kk_move[0] += 2
+        
+        kk_rct.move_ip(kk_move)
+        
+        x = tmr % 3200 #練習5
+        screen.blit(bg_img, [-x, 0]) #練習2,3
+        screen.blit(bg_img2, [-x + 1600, 0])#練習7
+        screen.blit(bg_img, [-x + 3200, 0]) #練習
+        screen.blit(kk_img, kk_rct) #練習4
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200) #練習6
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
