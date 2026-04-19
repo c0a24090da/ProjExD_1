a42c392e9a5fbd900a850c1eff5bb5fb4b1a3481
koseki <c0a24090da@edu.teu.ac.jp>
2026-04-19 20:03:43 +0900

    課題2:move_ipメソッドの応用

--- pygame_image.py
+++ pygame_image.py
@@ -21,16 +21,18 @@ def main():
             if event.type == pg.QUIT: return
         
         key_list = pg.key.get_pressed()
-        kk_rct.move_ip(-1, 0)
+        kk_move = [-1, 0]
 
         if key_list[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            kk_move[1] -= 1
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
+            kk_move[1] += 1
         if key_list[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            kk_move[0] -= 1
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip(2, 0)
+            kk_move[0] += 2
+        
+        kk_rct.move_ip(kk_move)
         
         x = tmr % 3200 #練習5
         screen.blit(bg_img, [-x, 0]) #練習2,3
